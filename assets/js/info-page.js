270989a7a9dc7f84671d8cb4c3ae595f3454b012
cFrost <cfrost.sun@live.com>
2025-02-18 23:11:27 +0800

    Create info-page.js

---
+++ assets/js/info-page.js
@@ -0,0 +1,11 @@
+function cleanUpTargetList(element) {
+  let actionElements = $(element).children('.action');
+  let actionCount = actionElements.length;
+  for (let actionElement of actionElements) {
+      $(actionElement).addClass('col-md-2')
+  }
+  let offset = (12 - 2 * actionCount) / 2;
+  if (offset > 0) {
+      $(actionElements[0]).addClass('col-md-offset-' + offset)
+  }
+}
