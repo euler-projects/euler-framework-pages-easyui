914956a242417802a2767920eab3982e76bddcb3
cFrost <cfrost.sun@live.com>
2025-02-21 23:19:10 +0800

    Update info-page.js

--- assets/js/info-page.js
+++ assets/js/info-page.js
@@ -1,11 +1,13 @@
-function cleanUpActions(element) {
-  let actionElements = $(element).children('.action');
-  let actionCount = actionElements.length;
-  for (let actionElement of actionElements) {
-      $(actionElement).addClass('col-md-2')
-  }
-  let offset = (12 - 2 * actionCount) / 2;
-  if (offset > 0) {
-      $(actionElements[0]).addClass('col-md-offset-' + offset)
+function cleanUpActions(elements) {
+  for(let element of elements) {
+    let actionElements = $(element).children('.action');
+    let actionCount = actionElements.length;
+    for (let actionElement of actionElements) {
+        $(actionElement).addClass('col-md-2')
+    }
+    let offset = (12 - 2 * actionCount) / 2;
+    if (offset > 0) {
+        $(actionElements[0]).addClass('col-md-offset-' + offset)
+    } 
   }
 }
